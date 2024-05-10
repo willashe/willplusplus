c4135939bcf7f48394df4153e4e03390f975ce65
Brant Ashe <brantashe@gmail.com>
2024-05-10 00:18:53 -0500

    rough copy of old site

---
+++ src/components/ContactInfo.tsx
@@ -0,0 +1,20 @@
+const ContactInfo = () => (
+  <div className="contact-info">
+    <a
+      href="http://www.linkedin.com/in/will-ashe"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      LinkedIn
+    </a>
+    <a
+      href="https://github.com/willashe"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      GitHub
+    </a>
+  </div>
+);
+
+export default ContactInfo;
