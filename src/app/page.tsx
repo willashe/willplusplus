a441e382ccfc781c4be580182e8c9e5403882be7
Brant Ashe <brantashe@gmail.com>
2024-05-10 00:27:18 -0500

    try to fix attack icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,10 +19,6 @@ export default function Home() {
         </div>
 
         <Skillset />
-
-        <a href="/rpg-battle" className={styles.attackIcon}>
-          RPG Battle Sim
-        </a>
       </div>
     </main>
   );
